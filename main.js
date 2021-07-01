89107bfef7bd17a3e0aeee4b653f6bbad909e299
amritgiri <amritgiri5813@gmail.com>
2021-07-01 19:35:59 +0545

    trying js

---
+++ main.js
@@ -0,0 +1,13 @@
+var menuList = document.getElementById("menuList");
+
+      menuList.style.maxHeight = "0px";
+
+      function togglemenu(){
+        if(menuList.style.maxHeight == "0px")
+        {
+          menuList.style.maxHeight = "350px";
+        }
+        else{
+          menuList.style.maxHeight = "0px";
+        }
+      } 
