e1e74779b07a9d15f104b07ab8b61897f57d7e35
amritgiri <amritgiri5813@gmail.com>
2021-07-10 20:47:52 +0545

    Addition of project

--- main.js
+++ main.js
@@ -11,3 +11,24 @@ var menuList = document.getElementById("menuList");
           menuList.style.maxHeight = "0px";
         }
       }
+
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+let project = document.getElementById("project_description1")
+
+project.style.color = "white";
