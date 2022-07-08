a9a311218e82f8a0da26ef4e0a2ea8e3fc1023e9
amrit <amrit@2400-1A00-BDE0.ip6.wlink.com.np>
2022-07-08 22:53:34 +0545

    change in contact

--- app.js
+++ app.js
@@ -11,3 +11,29 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+
+var btn = document.getElementById('btn');
+
+btn.addEventListener('click',function(e){
+    e.preventDefault()
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var number = document.getElementById('number').value;
+    var message = document.getElementById('message').value;
+
+    var body = 'name: '+ name+'<br/> email: '+ email + '<br/> phone number: '+ number + '<br/> message: '+ message; 
+
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "amritgiri5813@gmail.com",
+        Password : "sivabbsrzxdgirra",
+        To : 'amritgiri5813@gmail.com',
+        From : email,
+        Subject : number,
+        Body : body
+    }).then(
+      message => alert(message)
+    );
+})
